8b0cca6ca31b15fae1c0da3cd25069402497aba5
Benedikt <91166910+bndkt@users.noreply.github.com>
2023-06-06 14:34:26 +0800

    Add live activity control

--- example/App.tsx
+++ example/App.tsx
@@ -1,11 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  areActivitiesEnabled,
+  startActivity,
+  updateActivity,
+  endActivity,
+} from "react-native-widget-extension";
 
-export default function App() {
+export default function TabTwoScreen() {
+  const [driverName, setDriverName] = useState<string>("");
+  const [numberOfPizzas, setNumberOfPizzas] = useState<string>("");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Live Activities: {areActivitiesEnabled() ? "✅" : "❌"}</Text>
+      {areActivitiesEnabled() && (
+        <>
+          <View style={{ marginTop: 30, marginBottom: 15 }}>
+            <Text>Driver:</Text>
+            <TextInput
+              placeholder="Driver"
+              style={{
+                backgroundColor: "white",
+                borderColor: "lightgray",
+                borderWidth: 1,
+                padding: 5,
+                width: 200,
+              }}
+              value={driverName}
+              onChangeText={setDriverName}
+            />
+          </View>
+          <View style={{ marginBottom: 30 }}>
+            <Text>Number of pizzas:</Text>
+            <TextInput
+              placeholder="Number of pizzas"
+              style={{
+                backgroundColor: "white",
+                borderColor: "lightgray",
+                borderWidth: 1,
+                padding: 5,
+                width: 200,
+              }}
+              value={numberOfPizzas}
+              onChangeText={setNumberOfPizzas}
+            />
+          </View>
+          <Button
+            title="Start activity (and update after 10 sec)"
+            onPress={() => {
+              console.log("startActivity() pressed");
+              startActivity(
+                Number(numberOfPizzas),
+                "4343",
+                "$32.23",
+                driverName,
+                47,
+                43
+              );
+              setTimeout(() => {
+                updateActivity({
+                  driverName: "John",
+                  minutes: 12,
+                  seconds: 24,
+                });
+              }, 10000);
+            }}
+          />
+          <Button
+            title="Update activity"
+            onPress={() => {
+              console.log("updateActivity() pressed");
+              updateActivity({
+                driverName,
+                minutes: 12,
+                seconds: 24,
+              });
+            }}
+          />
+          <Button
+            title="End activity"
+            onPress={() => {
+              console.log("endActivity() pressed");
+              endActivity({ driverName });
+            }}
+          />
+        </>
+      )}
     </View>
   );
 }
@@ -13,8 +94,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 30,
+    height: 1,
+    width: "80%",
   },
 });
